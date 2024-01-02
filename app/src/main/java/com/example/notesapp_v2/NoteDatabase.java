970cc2eb611c02e0a038b4defb1172e053ebc0ac
NikitaGoncharick <nikitagoncharick@gmail.com>
2024-01-02 21:51:25 +0100

    update

--- app/src/main/java/com/example/notesapp_v2/NoteDatabase.java
+++ app/src/main/java/com/example/notesapp_v2/NoteDatabase.java
@@ -6,25 +6,24 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
-    private static NoteDatabase instnace;
+    private static NoteDatabase databse;
     public abstract NoteDao noteDao();
     private static final int NUMBER_OF_THREADS = 4;
 
     //Этот код создаёт пул из четырёх потоков для выполнения задач, связанных с базой данных.
     // Это позволяет нам выполнять операции вставки, обновления и удаления в фоновом режиме
-    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    static final ExecutorService databaseExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
-    public static synchronized NoteDatabase getInstance(Context context){
-        if (instnace == null){
-            instnace = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_databse")
+    public static synchronized NoteDatabase getDatabase(Context context){
+        if (databse == null){
+            databse = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_databse")
                     .fallbackToDestructiveMigration().build();
         }
-        return instnace;
+        return databse;
     }
 }
--- app/src/main/java/com/example/notesapp_v2/NoteRepository.java
+++ app/src/main/java/com/example/notesapp_v2/NoteRepository.java
@@ -6,21 +6,32 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
-//В этом классе мы создаём методы для вставки, обновления и удаления заметок.
-// Операции с базой данных выполняются асинхронно с использованием ExecutorService.
+//----------------------------------------------ОБРАБОТКА ЛОГИКИ NoteDAO-------------------------------------------------//
+//--------------------NoteRepository взаимодействует с базой данных через NoteDao и обновляет данные---------------------//
 
-public class NoteRepository {
+// NoteRepository - это как связующее звено между базой данных и остальной частью приложения.
+// Представьте, что NoteRepository это посредник, который берет на себя всю работу по общению с базой данных.
+// + NoteRepository позволяет абстрагироваться от конкретного источника данных!!
+// + NoteRepository может управлять асинхронными операциями
+
+public class  NoteRepository {
     private NoteDao noteDao;
-    private LiveData<List<Note>> allNotes;
+    private LiveData<List<Note>> allNotes;  // Представьте LiveData как магическую коробку, в которой хранятся все ваши заметки.
+                                             // Каждый раз, когда вы добавляете, удаляете или изменяете заметку в вашем приложении,
+                                            // магическая коробка автоматически обновляется с новым списком заметок
+
+                                            // Ваш пользовательский интерфейс (например, экран со списком заметок) "подписывается"
+                                            // на эту магическую коробку. Это означает, что он автоматически получает уведомления
+                                             // каждый раз, когда что-то в коробке меняется.
 
     public NoteRepository(Application application){
-        NoteDatabase database = NoteDatabase.getInstance(application);
-        noteDao = database.noteDao();
-        allNotes = noteDao.getAllNotes();
+        NoteDatabase database = NoteDatabase.getDatabase(application); // объявили базу данных
+        noteDao = database.noteDao(); // подключили запросы к базе данных
+        allNotes = noteDao.getAllNotes(); // наполнили ..волшебную коробку.. данными из базы данных
     }
 
     public void insert(Note note){
-        NoteDatabase.databaseWriteExecutor.execute(new Runnable() {
+        NoteDatabase.databaseExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 noteDao.insert(note);
@@ -28,7 +39,7 @@ public class NoteRepository {
         });
     }
     public void update(Note note){
-        NoteDatabase.databaseWriteExecutor.execute(new Runnable() {
+        NoteDatabase.databaseExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 noteDao.update(note);
@@ -36,13 +47,16 @@ public class NoteRepository {
         });
     }
     public void delete(Note note){
-        NoteDatabase.databaseWriteExecutor.execute(new Runnable() {
+        NoteDatabase.databaseExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 noteDao.delete(note);
             }
         });
     }
+
+    //метод возвращает объект LiveData, который содержит список (List) заметок (Note). LiveData - это особый тип данных в Android,
+    // который автоматически обновляется, когда изменяются данные внутри него.
     public LiveData<List<Note>>getAllNotes(){
         return allNotes;
     }
